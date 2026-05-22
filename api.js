862feba1ed81205f15597197106d1c06c1b0b57f
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-05-22 08:27:13 +0200

    change api.js by removing export at buttom

--- api.js
+++ api.js
@@ -1,15 +1,14 @@
-function fetchPosts(){
+export function fetchPosts() {
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(posts) {
-        console.log(posts);
-    })
-    .catch(function(error) {
-        console.error("Error fetching posts:", error);
-        return [];
-    })
+    return fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(posts) {
+            return posts;
+        })
+        .catch(function(error) {
+            console.error("Error fetching posts:", error);
+            return [];
+        });
 }
-fetchPosts();
